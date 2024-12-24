1c355e44c97abb13551252644f25e78696d07064
nakazye <nakazye@gmail.com>
2024-12-24 06:23:14 +0000

    add jetpack and colorscheme to nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -62,3 +62,17 @@ vim.keymap.set('i', '<C-n>', '<Down>', { remap = true })
 vim.keymap.set('i', '<C-f>', '<right>', { remap = true })
 vim.keymap.set('i', '<C-b>', '<left>', { remap = true })
 
+
+-- プラグインマネージャ(JetPack)
+local jetpackfile = vim.fn.stdpath('data') .. '/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim'
+local jetpackurl = 'https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim'
+if vim.fn.filereadable(jetpackfile) == 0 then
+  vim.fn.system('curl -fsSLo ' .. jetpackfile .. ' --create-dirs ' .. jetpackurl)
+end
+
+vim.cmd('packadd vim-jetpack')
+require('jetpack.packer').add {
+  'tani/vim-jetpack',
+  {'beikome/cosme.vim', run = vim.cmd[[colorscheme cosme]]}
+}
+
