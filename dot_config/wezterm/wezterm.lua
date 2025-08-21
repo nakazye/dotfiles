1cdc5207ea0b1a996fe97d1bdbe89b02e9208cdf
nakazye <nakazye@gmail.com>
2025-08-21 14:32:18 +0900

    諸々

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,5 +9,17 @@ config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
 config.show_tabs_in_tab_bar = false
 config.disable_default_key_bindings = true
 
+config.keys = {
+  {
+    key = 'v',
+    mods = 'CMD',
+    action = wezterm.action.PasteFrom 'Clipboard',
+  },
+  {
+    key = 'c',
+    mods = 'CMD',
+    action = wezterm.action.CopyTo 'Clipboard',
+  },
+}
 
 return config
