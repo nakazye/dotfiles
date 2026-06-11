6dc72f4ee74783cb5507cf9dac78e79ee829378e
nakazye <nakazye@gmail.com>
2026-06-11 19:06:19 +0900

    no-litteringのauto-save/backup/ディレクトリを起動時に自動作成

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -95,6 +95,8 @@
         :config
         ;; バックアップ・自動保存ファイルをvar/配下に配置
         (no-littering-theme-backups)
+        (make-directory (no-littering-expand-var-file-name "auto-save/") t)
+        (make-directory (no-littering-expand-var-file-name "backup/") t)
         ;; auto-save-listもvar/配下に配置
         (setq auto-save-list-file-prefix (no-littering-expand-var-file-name "auto-save-list/.saves-"))
         ;; カスタムテーマディレクトリをetc/配下に配置
